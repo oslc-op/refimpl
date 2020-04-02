b962443418ed0d084de9e303679991d9df2a613c
Andrew Berezovskyi <andriib@kth.se>
2020-04-03 00:53:37 +0200

    Add initial QM population over CF

--- src/client-toolchain/src/main/kotlin/Main.kt
+++ src/client-toolchain/src/main/kotlin/Main.kt
@@ -1,31 +1,42 @@
 import kotlinx.coroutines.*
+import org.eclipse.lyo.oslc.domains.qm.TestPlan
 import org.eclipse.lyo.oslc.domains.rm.Requirement
 import org.eclipse.lyo.oslc.domains.rm.RequirementCollection
 import org.eclipse.lyo.oslc4j.client.OslcClient
 import org.eclipse.lyo.oslc4j.core.annotation.OslcResourceShape
 import org.eclipse.lyo.oslc4j.core.model.*
+import java.net.ConnectException
 import java.net.URI
 import java.util.*
 import javax.ws.rs.core.Response
 import kotlin.collections.HashSet
 import kotlin.system.measureNanoTime
 
-private val NS_REQUIREMENT = URI.create("http://open-services.net/ns/rm#Requirement")
-private val NS_REQUIREMENT_COLL = URI.create("http://open-services.net/ns/rm#RequirementCollection")
-
-
 fun main() {
     println("Populating OSLC RefImpl servers with sample data.\n")
     val client = OslcClient()
 
+    populateRM(client)
+    populateQM(client)
+}
+
+
+fun populateRM(client: OslcClient) {
     val rmTraverser = ServiceProviderCatalogTraverser("http://localhost:8800/services/catalog/singleton", client)
     val reqPopulator = CreationFactoryPopulator(client, rmTraverser, 50, RandomResourceGen(::genRequirement),
             Requirement::class.java)
     reqPopulator.populate()
-    val reqCollPopulator = CreationFactoryPopulator(client, rmTraverser, 20,
+    val reqCollPopulator = CreationFactoryPopulator(client, rmTraverser, 30,
             RandomResourceGen(::genRequirementColl), RequirementCollection::class.java)
     reqCollPopulator.populate()
 }
+
+private fun populateQM(client: OslcClient) {
+    val qmTraverser = ServiceProviderCatalogTraverser("http://localhost:8802/services/catalog/singleton", client)
+    var planPopulator = CreationFactoryPopulator(client, qmTraverser, 50, RandomResourceGen(::genPlan), TestPlan::class.java)
+    planPopulator.populate()
+}
+
 
 class ServiceProviderCatalogTraverser(private val spCatalog: String, private val client: OslcClient) {
     private val providers: MutableSet<ServiceProvider> = HashSet()
@@ -73,7 +84,7 @@ class CreationFactoryPopulator<T : AbstractResource>(private val client: OslcCli
                 }
             }
         }
-        println("Created ${resources.size} in ${nanoTime/1000/1000}ms")
+        println("Created ${resources.size} in ${nanoTime / 1000 / 1000}ms")
     }
 
     private inline fun filterRelevantFactories(): List<Pair<ServiceProvider, CreationFactory>> {
@@ -97,33 +108,47 @@ class CreationFactoryPopulator<T : AbstractResource>(private val client: OslcCli
 fun <T : AbstractResource> postResources(client: OslcClient, cf: CreationFactory, sp: ServiceProvider, count: Int, generator: RandomResourceGen<T>, scope: CoroutineScope): Set<Link> {
     val createdUrls = HashSet<Link>()
     val resources = generator.generate(sp, count)
-    val responsesAsync: List<Deferred<Pair<Response, T>>> = resources.map { r ->
+    val responsesAsync: List<Deferred<Pair<Response?, T>>> = resources.map { r ->
         scope.async {
             val cfURI = cf.creation.toString()
-            val response = client.createResource(cfURI, r, "text/turtle")
-            if (response.status < 400) {
-                val headers = response.headers
-                val url: String? = headers["Location"]?.single() as String
-                if (url != null) {
-                    println("$url created via $cfURI")
-                } else {
-                    println("WARNING! A Resource was created but no Location was given")
-                }
+            var response: Response? = null
+            for (i in 1..5) { //retry up to 5 times
+                try {
+                    response = client.createResource(cfURI, r, "text/turtle")
+                    if (response != null) {
+                        if (response.status < 400) {
+                            val headers = response.headers
+                            val location: MutableList<Any>? = headers["Location"]
+                            if(location != null) {
+                                val url: String = location.single() as String
+                                println("$url created via $cfURI")
+                            } else {
+                                println("WARNING! A Resource was created but no Location was given")
+                            }
 
-            } else {
-                println("Failed to create a resource via $cfURI")
+                        } else {
+                            println("Failed to create a resource via $cfURI")
+                        }
+                        break
+                    }
+                } catch (e: ConnectException) {
+                    println("Connection failed, retrying... ($cfURI)")
+                }
             }
             Pair(response, r)
         }
     }
     runBlocking(scope.coroutineContext) {
-        val responses: List<Pair<Response, T>> = responsesAsync.awaitAll()
+        val responses: List<Pair<Response?, T>> = responsesAsync.awaitAll()
         responses.forEach { (response, _) ->
-            if (response.status < 400) {
-                val headers = response.headers
-                val url: String? = headers["Location"]?.single() as String
-                if (url != null) {
-                    createdUrls.add(Link(URI.create(url), "TBD"))
+            if (response != null) {
+                if (response.status < 400) {
+                    val headers = response.headers
+                    val location: MutableList<Any>? = headers["Location"]
+                    if(location != null) {
+                        val url: String = location.single() as String
+                        createdUrls.add(Link(URI.create(url), "TBD"))
+                    }
                 }
             }
         }
@@ -151,6 +176,17 @@ fun genRequirementColl(sp: ServiceProvider, id: Int): RequirementCollection {
         title = "Requirement Collection no. $id"
         description = "Requirement Collecion no. $id was generated by a 'client-toolchain' project. Edit its to change how these resources look."
         modified = Date()
+    }
+    return r
+}
+
+fun genPlan(sp: ServiceProvider, id: Int): TestPlan {
+    var r = TestPlan()
+    r.apply {
+        title = "Test Plan no. $id"
+        created = Date()
+        // TODO: 2020-04-02 set more fields
+        // TODO: 2020-04-02 find a way to link resources
     }
     return r
 }
