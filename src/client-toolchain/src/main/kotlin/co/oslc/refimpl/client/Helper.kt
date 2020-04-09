a114dd21deab1f5e2a9f16afb9fcc0807876402d
Andrew Berezovskyi <andriib@kth.se>
2020-04-10 01:26:27 +0200

    Add link deps in the client & refactor
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ src/client-toolchain/src/main/kotlin/co/oslc/refimpl/client/Helper.kt
@@ -0,0 +1,29 @@
+package co.oslc.refimpl.client
+
+import org.eclipse.lyo.oslc4j.core.model.Link
+import java.net.URI
+import kotlin.random.Random
+
+fun randomId(maxId: Int): Int {
+    val r = Random(System.nanoTime())
+    return r.nextInt(1, maxId)
+}
+
+fun randomIdExcept(maxId: Int, exceptId: Int): Int {
+    val r = Random(System.nanoTime())
+    var linkedId = r.nextInt(1, maxId)
+    while (linkedId == exceptId) {
+        linkedId = r.nextInt(1, maxId)
+    }
+    return linkedId
+}
+
+fun link(to: String): Link = Link(URI.create(to))
+fun link(to: URI): Link = Link(to)
+
+fun singleLinkTo(to: String) = setOf(link(to))
+fun singleLinkTo(to: URI) = setOf(link(to))
+
+fun randomLink(links: List<Link>): Set<Link> {
+    return setOf(links[randomId(links.size+1) - 1])
+}
