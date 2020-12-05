8fd62b2a551c56e1c2f758d59d796347471d8ad7
Andrew Berezovskyi <andriib@kth.se>
2020-12-05 19:58:54 +0100

    Add an RDF test

--- src/server-rm/src/test/java/SpCatalogIT.java
+++ src/server-rm/src/test/java/SpCatalogIT.java
@@ -2,10 +2,20 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.config.XmlConfig.xmlConfig;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import io.restassured.RestAssured;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.eclipse.lyo.oslc4j.core.model.ServiceProvider;
+import org.eclipse.lyo.oslc4j.core.model.ServiceProviderCatalog;
+import org.eclipse.lyo.oslc4j.provider.jena.JenaModelHelper;
 import org.hamcrest.*;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 public class SpCatalogIT {
 
@@ -36,6 +46,24 @@ public class SpCatalogIT {
             .statusCode(200)
             .contentType("application/rdf+xml");
     }
+
+    @Test
+    public void testContent() {
+        final InputStream inputStream = given()
+                .auth().basic("admin", "admin")
+                .accept("application/rdf+xml")
+            .when()
+                .get(URI_SPC)
+                .asInputStream();
+
+        Model m = ModelFactory.createDefaultModel();
+        RDFDataMgr.read(m, inputStream, Lang.RDFXML);
+
+        final ServiceProviderCatalog catalog = JenaModelHelper.unmarshalSingle(m, ServiceProviderCatalog.class);
+
+        assertEquals(URI_SPC, catalog.getAbout().toString());
+    }
+
 
     @Test
     public void testServesRootServices() {
